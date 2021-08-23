fdcce4b0e10bf1d8a2ef56bf557d722645849d3a
aflamiano-std <aflamiano.learning@gmail.com>
2021-08-23 16:25:26 +0800

    Fixed header navigation and bookmark toggle

--- main.js
+++ main.js
@@ -7,10 +7,29 @@ const overlay = document.getElementById("overlay");
 const continueButtons = document.querySelectorAll(".continue");
 const gotItButton = document.querySelector(".got-it");
 
+// Pledge radio button
+const radioButtons = document.querySelectorAll(".pledge__radio-input");
+
+radioButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    selectPledge(button);
+  });
+});
+
 selectRewardButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const modal = document.getElementById("modal-reward");
     openModal(modal);
+    if (button.classList.contains("bamboo")) {
+      selectPledge(radioButtons[1]);
+      modalFocus(600);
+    } else if (button.classList.contains("black")) {
+      selectPledge(radioButtons[2]);
+      modalFocus(850);
+    } else {
+      selectPledge(radioButtons[0]);
+      modalFocus(400);
+    }
   });
 });
 
@@ -20,6 +39,7 @@ continueButtons.forEach((button) => {
     closeModal(modal_rewards);
     const modal = document.getElementById("modal-thanks");
     openModal(modal);
+    modalFocus(800);
   });
 });
 
@@ -51,4 +71,42 @@ function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove("active");
   overlay.classList.remove("active");
+  if (radioButtons[1].checked) {
+    document.querySelectorAll(".product__title")[0].scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  } else if (radioButtons[2].checked) {
+    document.querySelectorAll(".product__title")[1].scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  }
 }
+
+function selectPledge(button) {
+  button.checked = true;
+  const pledge = button.closest(".pledge");
+  const allPledges = document.querySelectorAll(".pledge");
+  allPledges.forEach((pledge) => {
+    pledge.lastElementChild.classList.remove("show");
+    pledge.classList.remove("active");
+  });
+  pledge.classList.add("active");
+  pledge.lastElementChild.classList.add("show");
+}
+
+function modalFocus(x_coordinate) {
+  window.scrollTo({
+    top: x_coordinate,
+    behavior: "smooth",
+  });
+}
+
+/* HAMBURGER MENU */
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("nav--visible");
+});
